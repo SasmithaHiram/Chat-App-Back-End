4768d9c31e5268c17247c2365807dd6dcad423be
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-04-07 15:40:26 +0530

    Signup

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,47 @@
-export const signup = (req, res) => {
-  const {fullName, email, password} = req.body
+import { generateToken } from "../lib/util.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
   
+  const { fullName,email,password } = req.body;
+
   try {
-    
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "PLEASE MUST BE AT LEAST 6 CHARACTERS" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: "EMAIL ALREADY EXISTS" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedpassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedpassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "INVALID USER DATA" });
+    }
   } catch (error) {
-    
+    console.log("ERROR IN SIGNUP CONTROLLER", error.message);
+    res.status(500).json({ message: "INTERNAL SERVER ERROR" });
   }
 };
 
