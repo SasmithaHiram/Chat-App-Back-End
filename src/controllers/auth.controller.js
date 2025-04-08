a0112254322061fafae655e217c5d9bf7046b3bf
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-04-08 12:10:29 +0530

    Implement Profile

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/util.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -85,4 +86,24 @@ export const logout = (req, res) => {
   }
 };
 
-export const updateProfile = async (req, res) => {};
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(300).json({ message: "PROFILE PIC IS REQUIRED" });
+    }
+
+    const uplodeResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findById(
+      userId,
+      { profilePic: uplodeResponse.secure_url },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("ERROR IN UPDATE PROFILE", error);
+    res.status(500).json({ message: "INTERNAL SERVER ERROR" });
+  }
+};
