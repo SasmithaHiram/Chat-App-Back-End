01534235b04163d303b0e13a6351d9deb18f3590
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-04-08 19:52:13 +0530

    New Message

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -33,3 +33,31 @@ export const getMessages = async (req, res) => {
     res.status(500).json({ error: "INTERNAL SERVER ERROR" });
   }
 };
+
+export const sendMessage = async (requestAnimationFrame, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverdId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uplodeResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uplodeResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverdId,
+      text,
+      image: imageURL,
+    });
+
+    await newMessage.save();
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("ERROR IN SEND MESSAGE CONTROLLER", error.message);
+    res.status(500).json({ error: "INTERNAL SERVER ERROR" });
+  }
+};
