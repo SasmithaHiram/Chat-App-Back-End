b313ac3011ad10fd86de545a107ecb20efd1d8fd
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-04-12 10:22:17 +0530

    Fix Message Controller Error

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -39,34 +39,30 @@ export const getMessages = async (req, res) => {
 
 export const sendMessage = async (req, res) => {
   try {
-    const { text, image } = req.body;
-    const { id: receiverId } = req.params;
+    const receiverId = req.params.id;
     const senderId = req.user._id;
+    const { text, image } = req.body;
 
-    let imageUrl;
-    if (image) {
-      // Upload base64 image to cloudinary
-      const uploadResponse = await cloudinary.uploader.upload(image);
-      imageUrl = uploadResponse.secure_url;
-    }
-
-    const newMessage = new Message({
+    const message = await Message.create({
       senderId,
       receiverId,
       text,
-      image: imageUrl,
+      image,
     });
-
-    await newMessage.save();
 
     const receiverSocketId = getReceiverSocketId(receiverId);
     if (receiverSocketId) {
-      io.to(receiverSocketId).emit("newMessage", newMessage);
+      io.to(receiverSocketId).emit("newMessage", message);
     }
 
-    res.status(201).json(newMessage);
-  } catch (error) {
-    console.log("Error in sendMessage controller: ", error.message);
-    res.status(500).json({ error: "Internal server error" });
+    const senderSocketId = getReceiverSocketId(senderId);
+    if (senderSocketId && senderSocketId !== receiverSocketId) {
+      io.to(senderSocketId).emit("newMessage", message);
+    }
+
+    res.status(201).json(message);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Failed to send message" });
   }
 };
